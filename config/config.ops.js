ff8f8d6f1676d5dc4eea24cedbc4adae59b5dd89
hank <anhui3713@vip.qq.com>
2019-11-14 23:40:48 +0800

    change port to 443

--- config/config.ops.js
+++ config/config.ops.js
@@ -1,11 +1,11 @@
 const isProd = process.env.NODE_ENV === 'production'
-
+const port = process.env.PORT
 /**
  * 运维配置文件, 非开发环境会屏蔽此文件
  */
 module.exports = {
   // 前端服务默认端口号
-  DEFUALT_PORT: 3001,
+  DEFUALT_PORT: port || 443,
   // cdn地址
   CDN_URL: isProd ? 'http://localhost:3800' : '',
   // 接口地址
